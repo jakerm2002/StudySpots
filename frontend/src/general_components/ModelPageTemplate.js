af3159f84d8001aac9a740ec547cf99d5245b1fd
Jake Medina <jake.medina02@gmail.com>
2022-10-02 13:57:59 -0500

    rudimentary model page and css

--- frontend/src/general_components/ModelPageTemplate.js
+++ frontend/src/general_components/ModelPageTemplate.js
@@ -1,47 +1,62 @@
-//import Table from 'react-bootstrap/Table';
+import Table from 'react-bootstrap/Table';
+import React from 'react'
 import JsonData from '../api_resources/coffeeshops.json'
+import './ModelPageTemplate.css'
 
 function ModelPageTemplate(){
-    const Dis = JsonData.map(
+    console.log(JsonData);
+    const Businesses = JsonData.businesses.map(
         (info) => {
+            var open = "Open";
+            if (info.is_closed) {
+                open = "Closed";
+            }
             return(
                 <tr>
-                    <td>{info.id}</td>
-                    <td>{info.name}</td>
-                    <td>{info.city}</td>
+                    <td title={info.name}>{info.name}</td>
+                    <td title={info.location.city}>{info.location.city}</td>
+                    <td title={info.price}>{info.price}</td>
+                    <td title={info.rating}>{info.rating} ({info.review_count})</td>
+                    <td title={!info.is_closed ? "open" : "closed"} id={!info.is_closed ? "open" : "closed"}>{open}</td>
                 </tr>
             )
         }
     )
  
     return(
-        // <Table striped bordered hover>
-        //          <thead>
-        //              <tr>
-        //              <th>Name</th>
-        //                 <th>Address</th>
-        //              <th>Rating</th>
-        //              <th>Price</th>
-        //              <th>Hours</th>
-        //              <th>Phone #</th>
-        //              </tr>
-        //          </thead>
-        //      </Table>
-        <div>
-            <table class="table table-striped">
-                <thead>
-                    <tr>
-                    <th>Name</th>
-                    {/* <th>Name</th>
-                    <th>City</th> */}
-                    </tr>
-                </thead>
-                <tbody>
-                    {Dis}
-                </tbody>
-            </table>
+        <div className='list'>
+            <Table striped bordered hover>
+                    <thead>
+                        <tr>
+                        <th>Name</th>
+                        <th>City</th>
+                        <th>Price</th>
+                        <th>Rating</th>
+                        <th>Open/Closed</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {Businesses}
+                    </tbody>
+                </Table>
+            </div>
+        // <div>
+        //     <table className="table table-striped">
+        //         <thead>
+        //             <tr>
+        //             <th>Name</th>
+        //             <th>City</th>
+        //             <th>Price</th>
+        //             <th>Rating</th>
+        //             <th>Open/Closed</th>
+        //             </tr>
+        //         </thead>
+        //         <tbody>
+        //             {Businesses}
+        //         </tbody>
+        //     </table>
              
-        </div>
+        // </div>
     )
  }
  
