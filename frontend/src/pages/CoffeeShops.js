b9f992582a494ce0f45650c10f0237c64fb732d7
Jake Medina <jake.medina02@gmail.com>
2022-10-01 23:51:02 -0500

    removed unnecessary changes

--- frontend/src/pages/CoffeeShops.js
+++ frontend/src/pages/CoffeeShops.js
@@ -1,34 +1,4 @@
-// import Table from 'react-bootstrap/Table';
-import Dis from '../general_components/ModelPageTemplate'
-
 const CoffeeShops = () => {
-    return (
-        <div className="wrapper-all">
-            <h1>Coffee Shops</h1>
-            <Dis/>
-        </div>
-    )   
 }
-
-// const CoffeeShops = () => {
-//     return (
-//         <div className="wrapper-all">
-//             <h1>Coffee Shops</h1>
-
-//             <Table striped bordered hover>
-//                 <thead>
-//                     <tr>
-//                     <th>Name</th>
-//                     <th>Address</th>
-//                     <th>Rating</th>
-//                     <th>Price</th>
-//                     <th>Hours</th>
-//                     <th>Phone #</th>
-//                     </tr>
-//                 </thead>
-//             </Table>
-//         </div>
-//     )   
-// }
 
 export default CoffeeShops;
