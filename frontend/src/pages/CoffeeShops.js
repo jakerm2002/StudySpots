3f1d452de4c8a99a7e4880d7875e550f0a67264a
joannejchen <chenjjoanne@gmail.com>
2022-10-01 23:25:17 -0500

    move instance page template to branch

--- frontend/src/pages/CoffeeShops.js
+++ frontend/src/pages/CoffeeShops.js
@@ -1,10 +1,4 @@
 const CoffeeShops = () => {
-	var payload = {
-		"Image" : "https://i1.wp.com/www.agirlfromtx.com/wp-content/uploads/2019/05/coffee-shops-Austin-flat-track-coffee.jpg?resize=1595%2C1196&ssl=1",
-		"Stats" : ["Item 1: ", "Item 2:"],
-		"Body" : "Lorem Ipsum or whatever they say"
-	}
-	return getInstance(payload)
 }
 
 export default CoffeeShops;
