bee666482908f72b7b6f032091ca7d22de30fa3a
Bianca Alvarado <bianca.alvarado@utexas.edu>
2022-10-02 22:17:17 -0500

    fixing styles issue on pipeline

--- frontend/src/pages/Splash.js
+++ frontend/src/pages/Splash.js
@@ -5,7 +5,7 @@ import Card from 'react-bootstrap/Card';
 import logo from '../images/StudySpotsCircle.png';
 import { Row } from "react-bootstrap";
 import {Link} from 'react-router-dom';
-import styles from './Splash.module.css';
+import './Splash.css';
 
 const intervalTime = 10000; 
 
