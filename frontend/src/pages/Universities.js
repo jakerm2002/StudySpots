cd9737485574d2afc501dca5bd8aaa00438f3954
Bianca Alvarado <bianca.alvarado@utexas.edu>
2022-10-22 19:42:28 -0500

    added uni api call populated table | missing zip | error when adding tuition

--- frontend/src/pages/Universities.js
+++ frontend/src/pages/Universities.js
@@ -1,24 +1,36 @@
-import React from 'react'
-import api_results from '../api_resources/universities.json'
+import React, { useEffect, useState } from 'react'
 import getModel from '../general_components/ModelPageTemplate';
-import { Link } from 'react-router-dom';
+import axios from "axios";
 
 var currencyFormat = {style: 'currency', currency: 'USD', minimumFractionDigits: 0}
 var populationFormat = {style: 'decimal', minimumFractionDigits: 0}
 
 
 const Universities = () => {
-    const Entries = api_results.results.map(
+
+    const [universities, setUniversities] = useState([]);
+
+    useEffect(() => {
+        axios.get('http://api.studyspots.me/universities').then(response => {
+            console.log("response",response.data);
+            setUniversities(response.data);
+            
+        },
+        reject => {
+            console.log("REJECT");
+        });
+    }, []);
+
+    const Entries = universities.map(
         (info) => {
             return(
                 <tr onClick={() => window.location.href = `/Universities/${info.id}`}>
-                    <td title={info.latest.school.name}>{info.latest.school.name}</td>
-                    <td title={info.latest.school.city}>{info.latest.school.city}</td>
-                    <td title={info.latest.school.zip}>{info.latest.school.zip}</td>
-                    <td title={info.latest.student.size}>{info.latest.student.size.toLocaleString("en-US", populationFormat)}</td>
-                    <td title={info.latest.student.size}>{info.latest.cost.tuition.in_state.toLocaleString("en-US", currencyFormat)}</td>
-                    <td title={info.latest.student.size}>{info.latest.cost.tuition.out_of_state.toLocaleString("en-US", currencyFormat)}</td>
-                    {/* <Link to={`/Universities/${info.id}`}>Link</Link> */}
+                    <td title={info.name}>{info.name}</td>
+                    <td title={info.city}>{info.city}</td>
+                    <td title={info.zip}>{info.zip}</td>
+                    <td title={info.size}>{info.size.toLocaleString("en-US", populationFormat)}</td>
+                   {/* <td title={info.size}>{info.cost.tuition.in_state.toLocaleString("en-US", currencyFormat)}</td>
+                    <td title={info.size}>{info.tuition.out_of_state.toLocaleString("en-US", currencyFormat)}</td>  */}
                 </tr>
             )
         }
