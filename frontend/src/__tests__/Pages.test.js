cf81f1de79f61b0308cd20bd40423e67631e44ae
joannejchen <chenjjoanne@gmail.com>
2022-10-24 21:30:52 -0500

    basic rendering tests + technical report

--- frontend/src/__tests__/Pages.test.js
+++ frontend/src/__tests__/Pages.test.js
@@ -3,37 +3,64 @@ import About from "../pages/About"
 import CoffeeShops from "../pages/CoffeeShops"
 import Libraries from "../pages/Libraries"
 import Universities from "../pages/Universities"
-import InstanceCoffee from "../general_components/InstanceCoffee"
+import Splash from "../pages/Splash"
 import Navigation from "../general_components/Navigation"
 import Paginate from "../general_components/Pagination"
-import renderer, { act } from 'react-test-renderer'
+import { render, screen, waitFor, cleanup } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import '@testing-library/jest-dom'
+import { BrowserRouter } from "react-router-dom"
 
-it("Renders About page", async () => {
-    const tree = renderer.create(<About/>).toJSON();
-    expect(tree).toMatchSnapshot();
+afterEach(cleanup);
+
+describe("Test Navigation Bar", () => {
+    test("Proper Rendering of Navigation Component", () => {
+        render(<Navigation/>);
+        
+        // Make sure all of the navigation bars are included
+        expect(screen.getByText("Home")).toBeInTheDocument();
+        expect(screen.getByText("Coffee Shops")).toBeInTheDocument();
+        expect(screen.getByText("Libraries")).toBeInTheDocument();
+        expect(screen.getByText("Universities")).toBeInTheDocument();
+        expect(screen.getByText("About")).toBeInTheDocument();
+    });
+
 });
 
-it("Renders Coffee Shops page", async () => {
-    const tree = renderer.create(<CoffeeShops/>).toJSON();
-    expect(tree).toMatchSnapshot();
+describe("Test About Page", () => {
+    test("Proper Rendering of About Page", () => {
+        const originalError = console.error;
+        console.error = jest.fn();
+        render(<About/>);
+
+        // Make sure all the parts of the About page is included
+        expect(screen.getByText("StudySpots")).toBeInTheDocument();
+        expect(screen.getByText("Mission")).toBeInTheDocument();
+        expect(screen.getByText("Discovery")).toBeInTheDocument();
+        expect(screen.getByText("Meet the Team")).toBeInTheDocument();
+        expect(screen.getByText("Technology Used")).toBeInTheDocument();
+        expect(screen.getByText("APIs Scraped")).toBeInTheDocument();
+        expect(screen.getByText("Project Links")).toBeInTheDocument();
+
+        console.error = originalError;
+
+    });
 });
 
-it("Renders Libraries page", async () => {
-    const tree = renderer.create(<Libraries/>).toJSON();
-    expect(tree).toMatchSnapshot();
-});
+describe("Test Splash Page", () => {
+    it("Proper Rendering of Splash Page", () => {
+        const originalError = console.error;
+        console.error = jest.fn();
+        render(<Splash/>, {wrapper: BrowserRouter});
+        
+        // Make sure all the parts of the Splash page is included
+        expect(screen.getByText("Model Cards")).toBeInTheDocument();
+        expect(screen.getByText("Find your university!")).toBeInTheDocument();
+        expect(screen.getByText("Find coffee shops near you!")).toBeInTheDocument();
+        expect(screen.getByText("Find Libraries near you!")).toBeInTheDocument();
+        expect(screen.getByText("Meet Our Team")).toBeInTheDocument();
 
-it("Renders Universities page", async () => {
-    const tree = renderer.create(<Universities/>).toJSON();
-    expect(tree).toMatchSnapshot();
-});
+        console.error = originalError;
+    });
 
-it("Renders Navigation bar", async () => {
-    const tree = renderer.create(<Navigation/>).toJSON();
-    expect(tree).toMatchSnapshot();
-});
-
-it("Renders Pagination Component", async () => {
-    const tree = renderer.create(<Paginate/>).toJSON();
-    expect(tree).toMatchSnapshot();
 });
