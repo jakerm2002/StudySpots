70301f0a84c58cfc0d066291f5a8263dab680201
Jake Medina <jake.medina02@gmail.com>
2022-11-04 13:37:37 -0500

    sort options for universities

---
+++ frontend/src/general_components/UniversityOptions.js
@@ -0,0 +1,35 @@
+// to store options for frontend dropdown sort menu
+
+const UniversitySortOptions = [
+    {
+      label: "Name",
+      db_label: "name",
+    },
+    {
+      label: "Undergraduate Population",
+      db_label: "enrollment_ugr_12m",
+    },
+    {
+      label: "In-State Tuition",
+      db_label: "instate_tuition",
+    },
+    {
+      label: "Out-Of-State Tuition",
+      db_label: "outstate_tuition",
+    },
+    {
+      label: "SAT Median Math",
+      db_label: "sat_median_math",
+    },
+    {
+      label: "SAT Median Reading",
+      db_label: "sat_median_reading",
+    },
+    {
+      label: "Acceptance Rate",
+      db_label: "acceptance_rate",
+    }
+  ];
+  
+
+export { UniversitySortOptions };
