ed91d84fc5001fdaae988a6db6a17216ed9c7783
Ami Iyer <amrithaiyer02@utexas.edu>
2022-11-14 22:34:06 -0600

    sort arrow bug

--- frontend/src/general_components/Sort.js
+++ frontend/src/general_components/Sort.js
@@ -28,77 +28,52 @@ function Sorter(props) {
   const [sortOption, setSortOption] = React.useState(
     props.sortOptions[0]
   )
-  const [sortAscending, setSortAscending] = React.useState(props.sortAscending);
+  const [sortAscending, setSortAscending] = React.useState(
+    props.sortAscending
+  );
+
   var sortOptions = props.sortOptions;
 
   const handleSortChange = (option) => {
-    console.log("handling change")
-    let sortAscending = true
-    setSortAscending(sortAscending); // let searchParamsString = searchParams.toString();
-
-    let sortFilter = searchParams.get("sortBy");
-    if (sortFilter !== null) {
-      setSortOption(
-        sortOptions.find((o) => o.field === sortFilter) ??
-          sortOptions[0]
-      );
-    } else {
-      setSortOption(sortOptions[0]);
-    }
+    // console.log(sortAscending)
+    // let searchParamsString = searchParams.toString();
+    // let sortFilter = searchParams.get("sortBy");
+    // if (sortFilter !== null) {
+    //   setSortOption(
+    //     sortOptions.find((o) => o.field === sortFilter) ??
+    //       sortOptions[0]
+    //   );
+    // } else {
+    //   setSortOption(sortOptions[0]);
+    // }
     let newParams = searchParams;
     newParams.set("sortBy", option);
+    setSortAscending("true"); 
     newParams.delete("page");
     setSearchParams(newParams);
   };
 
-  const submitSearch = (newSearchQuery) => {
-    let newParams = searchParams;
-    if (newSearchQuery.length === 0) {
-        newParams.delete("search")
-    } else {
-        newParams.set("search", newSearchQuery);
-    }
-    newParams.set("page", 1);
-    setSearchParams(newParams);
-    submitSearch(searchQuery);
-};
+//   const submitSearch = (newSearchQuery) => {
+//     console.log(searchParams)
+//     let newParams = searchParams;
+//     if (newSearchQuery.length === 0) {
+//         newParams.delete("search")
+//     } else {
+//         newParams.set("search", newSearchQuery);
+//     }
+//     newParams.set("page", 1);
+//     setSearchParams(newParams);
+//     submitSearch(searchQuery);
+// };
 
   const handleSortDirectionChange = (sortAscending) => {
     let newParams = searchParams;
     newParams.set("ascending", sortAscending ? "true" : "false");
+    setSortAscending(sortAscending)
     newParams.delete("page");
     setSearchParams(newParams);
   };
 
-
-  // function getMenuOptions (options) {
-
-  //   menuOptions = []
-  //   for (var i = 0; i < options.length; i++) {
-  //     console.log(options[i])
-  //     menuOptions.push(<MenuItem
-  //       value={options[i].db_label}
-  //       onChange={(event) => 
-  //         handleSortChange(event.target.value)
-  //       }
-
-  //     >{options[i].label}</MenuItem>)
-  //   }
-  //   return menuOptions;
-  // };
-
-
-  // let defaultSortOptionIndex = props.defaultSortOptionIndex;
-
-  // useEffect(() => {
-  //   // setSearchQuery(searchParams.get("query") ?? "");
-
-    
-  //   // setSearchParams(newParams);
-  //   // setSearchQuery(searchParams.get("sat_median_math"));
-  // }, [searchParams, sortOptions]);
-
-	  
 	return (
 	<Container
 	  className="page-container"
@@ -125,20 +100,18 @@ function Sorter(props) {
 	    Sort
 	  </Typography>
 	  <Select
-    defaultValue = {sortOptions[0].db_label}
+    defaultValue = "Sort By"
 		children={sortOptions.map((option) => (
         <MenuItem
           value={option.db_label}
-          onChange={(event) => 
-            handleSortChange(event.target.value)
-          }>
+          >
           {option.label}</MenuItem>
       ))}
-    onKeyDown={(event) => {
-      if (event.key === "Enter") {
-        submitSearch(searchQuery);
-      }
-    }}
+    // onKeyDown={(event) => {
+    //   if (event.key === "Enter") {
+    //     submitSearch(searchQuery);
+    //   }
+    // }}
     onChange={(event) => handleSortChange(event.target.value)}
 	  />
 	</Container>
