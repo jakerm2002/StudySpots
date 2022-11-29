618d2012cc77463878ed7dbd26dbc3b47342d3c9
joannejchen <chenjjoanne@gmail.com>
2022-11-28 21:42:49 -0600

    sliders for countries visualizations

--- frontend/src/visualizations/Countries.js
+++ frontend/src/visualizations/Countries.js
@@ -1,9 +1,12 @@
 import { useEffect, useState } from "react";
-import { Box, Typography, Stack } from '@mui/material';
+import { Box, Typography, Slider, Stack } from '@mui/material';
 import { CartesianGrid, Label, ResponsiveContainer, Scatter, ScatterChart, Tooltip, XAxis, YAxis } from "recharts";
 import axios from "axios";
 
 const Countries = () => {
+    const [allData, setAllData] = useState([]);
+    const [areaRange, setAreaRange] = useState([0, 18000]);
+    const [populationRange, setPopulationRange] = useState([0, 1450]);
     const [data, setData] = useState([]);
 
     useEffect(() => {
@@ -32,6 +35,9 @@ const Countries = () => {
                     })
                 }
             });
+            setAllData(temp);
+            setAreaRange([0, 18000]);
+            setPopulationRange([0, 1450]);
             setData(temp);
         });
     }, []);
@@ -54,22 +60,67 @@ const Countries = () => {
         }
     }
 
+    function updateData() {
+        let temp = [];
+        for (let i = 0; i < allData.length; i++) {
+            if (allData[i]["area"] >= areaRange[0] && allData[i]["area"] <= areaRange[1]
+             && allData[i]["population"] >= populationRange[0] && allData[i]["population"] <= populationRange[1]) {
+                temp.push(allData[i]);
+            }
+        }
+        return temp;
+    }
+
+    const handleAreaChange = (event, newAreaRange) => {
+        setAreaRange(newAreaRange);
+        let temp = updateData();
+        setData(temp);
+    }
+    
+    const handlePopulationChange = (event, newPopulationRange) => {
+        setPopulationRange(newPopulationRange);
+        let temp = updateData();
+        setData(temp);
+    }
+
     return <>
     <Stack justifyContent="center">
         <h3>Area vs Population of a Country</h3>
+        <Box>
+            <h6>Area Range</h6>
+            <Slider
+                sx={{width: '50%'}}
+                getAriaLabel={() => "Area Range"}
+                value={areaRange}
+                onChange={handleAreaChange}
+                valueLabelDisplay="auto"
+                min={0}
+                max={18000}
+            />
+            <h6>Population Range</h6>
+            <Slider
+                sx={{width: '50%'}}
+                getAriaLabel={() => "Population Range"}
+                value={populationRange}
+                onChange={handlePopulationChange}
+                valueLabelDisplay="auto"
+                min={0}
+                max={1450}
+            />
+        </Box>
         <ResponsiveContainer width="100%" height={450}>
             <ScatterChart margin={{
                 right: 50, left: 50
             }}>
                 <CartesianGrid/>
-                <XAxis type="number" dataKey="area" height={40}>
+                <XAxis type="number" dataKey="area" height={40} domain={areaRange}>
                     <Label
                         value="Area of the Country (in 1000 km^2)"
                         position="insideBottom"
                         style={{textAnchor: "middle"}}
                     />
                 </XAxis>
-                <YAxis type="number" dataKey="population" domain={[0, 1000]}>
+                <YAxis type="number" dataKey="population" domain={populationRange}>
                     <Label
                         value="Population of the Country (in millions)"
                         angle={-90}
