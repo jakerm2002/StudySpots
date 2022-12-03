24cd05f2ab3726536ee985c5483a83b690254a84
Jake Medina <jake.medina02@gmail.com>
2022-12-02 19:42:57 -0600

    refactor UniversitySelect

--- frontend/src/general_components/UniversitySelect.js
+++ frontend/src/general_components/UniversitySelect.js
@@ -1,34 +1,17 @@
-//average coffee shop rating by state
-//OR
-//distribution of coffee shop ratings
-
-
-//pick nearby university and get bar chart of coffee shop star ratings
-
-
-//most reviewed coffee shops per university
-
-//make the nearby api return all coffee shops within 25 miles of LAT and LONG
-//with no limit to the number of results
-
-//add autocomplete for university names
-
 import { useEffect, useState, useMemo, Fragment } from "react";
-import { Box, Typography, Stack, Autocomplete, TextField, CircularProgress } from '@mui/material';
+import { Box, Autocomplete, TextField, CircularProgress } from '@mui/material';
 import axios from "axios";
-import { useSearchParams } from 'react-router-dom';
 import { UniversityEndpointName, UniversityDropdown } from './UniversityOptions';
 import throttle from "lodash/throttle";
 
 const UniversitySelect = (props) => {
 
     const [options, setoptions] = useState([]);
-    
-    const [inputValue, setInputValue] = useState("");
     const [open, setOpen] = useState(false);
     const loading = open && options.length === 0;
 
     useEffect(() => {
+        console.log("useeffect");
         let active = true;
         if (!loading) {
             return undefined;
@@ -41,18 +24,11 @@ const UniversitySelect = (props) => {
         };
     }, [loading]);
 
-    useEffect(() => {
-        if (!open) {
-            setoptions([]);
-        }
-    }, [open]);
 
     const fetchOptions = useMemo(() =>
         throttle(async () => {
-            setoptions([]);
-
             let response = await axios.get(
-                `https://api.studyspots.me/${UniversityEndpointName}/${UniversityDropdown.api}?query=` + inputValue
+                `https://api.studyspots.me/${UniversityEndpointName}/${UniversityDropdown.api}`
             );
             let data = response.data;
             let names = [];
@@ -63,7 +39,7 @@ const UniversitySelect = (props) => {
             setoptions(names);
 
         }, 200),
-        [inputValue]
+        []
     );
 
     return <>
@@ -82,12 +58,8 @@ const UniversitySelect = (props) => {
                         </Fragment>
                     ),
                 }} />}
-
                 onChange={(event, value) => {
                     props.setCurrentUniversity(value);
-                }}
-                onInputChange={(event, value) => {
-                    setInputValue(value);
                 }}
                 open={open}
                 onOpen={() => {
